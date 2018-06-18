dfa3f43d3a4e7cf9e779f5344636dff226436cfb
Krzysztof Owsiany <gocom@o2.pl>
2018-06-18 23:54:39 +0200

    Add EventFlowExample

---
+++ eventflow-example/Controllers/ValuesController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eventflow_example.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller {
+        private IMediator _mediator;
+
+        public ValuesController(IMediator mediator) {
+            _mediator = mediator;
+        }
+        // GET api/values
+        [HttpGet("{text}")]
+        public async Task<string> Get(string text) {
+            return await _mediator.Send(new GetDataQuery(text));
+        }
+
+        // POST api/values
+        [HttpPost]
+        public async void Post([FromBody] SetDataCommand command) {
+            await _mediator.Send(command);
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{key}")]
+        public async void Delete(string key) { 
+             await _mediator.Send(new DeleteDataCommand(key));
+        }sss
+    }
+}
